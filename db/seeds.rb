5e60db7f3234a5eed5689af49bc9bf84909f683e
gheorghe <giony.mirca@gmail.com>
2020-03-24 17:27:16 +0200

    'annotate'

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+article_list = [
+  [ "Edward Michael GryllsFrance", 'Edward Michael Grylls OBE (born 7 June 1974),
+  better known as Bear Grylls, is a British former SAS serviceman, survival
+  instructor, and honorary lieutenant-colonel, and, outside his military career,
+  an adventurer, writer, television presenter and businessman.' ],
+  [ "William Shakespeare", ' was an English poet, playwright, and actor, widely
+  regarded as the greatest writer in the English language and the world \' He is
+  often called England\'s national poet and the "Bard of Avon'],
+  [ "Nicolaus Copernicus", 'was a Renaissance-era polymath whose theory of the
+  universe placed the Sun rather than Earth at the center of the universe, in all
+  likelihood independently of Aristarchus of Samos, who had articulated similar
+  ideas some eighteen centuries earlier.']
+]
+
+article_list.each do |name, description|
+  Article.create( title: name, text: description )
+end
