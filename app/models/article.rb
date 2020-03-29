ea261d1c190f7f3490d0dfa4769588d74447c7da
gheorghe <giony.mirca@gmail.com>
2020-03-29 21:17:15 +0300

    bootstrap

--- app/models/article.rb
+++ app/models/article.rb
@@ -9,7 +9,12 @@
 #  updated_at :datetime         not null
 #
 class Article < ApplicationRecord
-    has_many :comments, dependent: :destroy
-    validates :title, presence: true,
-                      length: { minimum: 5 }
+    has_many :comments,
+    dependent: :destroy
+    validates :title,
+    presence: true,
+    length: { minimum: 5 }
+    validates :text,
+    presence: true,
+    length: { minimum: 5 }
 end
